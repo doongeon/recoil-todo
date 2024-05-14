31c4106ad35412ae164fd382ce996c0df532404b
donggeon Na <ndmb2012@gmail.com>
2024-05-14 13:29:57 +0900

    refactor: todo items

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,14 +1,60 @@
-import { useRecoilValue } from "recoil";
-import { filteredTodosState } from "../lib/atom";
-import TodoItem from "./TodoItem";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  Todo,
+  TodoCategory,
+  filteredTodosState,
+  todosState,
+} from "../lib/atom";
+
+const TodoCategories = [
+  TodoCategory.TO_DO,
+  TodoCategory.DOING,
+  TodoCategory.DONE,
+];
 
 export default function TodoItems() {
   const todos = useRecoilValue(filteredTodosState);
+  const setTodos = useSetRecoilState(todosState);
+
+  const updateTodoCategory = (oldTodo: Todo, newCategory: TodoCategory) => {
+    setTodos((prevTodos) => {
+      const idx = prevTodos.indexOf(oldTodo);
+      const newTodo = { ...oldTodo };
+      newTodo.category = newCategory;
+
+      return [...prevTodos.slice(0, idx), newTodo, ...prevTodos.slice(idx + 1)];
+    });
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.filter((todo) => todo.id !== todoId);
+      return newTodos;
+    });
+  };
 
   return (
     <div className="todoItems">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <div className="todoItem">
+          <span>{todo.todo}</span>
+          {TodoCategories.map((category) => {
+            if (category !== todo.category) {
+              return (
+                <button
+                  className="btn"
+                  key={category}
+                  onClick={() => updateTodoCategory(todo, category)}
+                >
+                  {category}
+                </button>
+              );
+            } else {
+              return null;
+            }
+          })}
+          <button onClick={() => deleteTodo(todo.id)}>delete</button>
+        </div>
       ))}
     </div>
   );
